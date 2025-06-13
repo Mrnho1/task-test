6c09d40576998d6022feba8a29b3b73ae5aa937f
lucas <ls.marinho36@gmail.com>
2025-06-13 14:07:19 -0300

    chore: inicializar projeto API com configurações básicas no application.properties

---
+++ src/test/java/com/dio/task_deploy/TaskDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.task_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
